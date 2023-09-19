df4c49cbe5944561018650ab16e7e2247a343df7
ZakharGirenko <106384009+ZakharGirenko@users.noreply.github.com>
2023-09-19 19:38:01 +0500

    Delete ITprof directory

--- ITprof/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-import app.models 
-
-admin.site.register(app.models.HomePageInfo)
-admin.site.register(app.models.RelevancePageInfo)
-admin.site.register(app.models.GeographyPageInfo)
-admin.site.register(app.models.SkillsPageInfo)
